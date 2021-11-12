30adb6b3f1da65b4bc65df5c934c13462576e4bf
Juan Esteban Deossa Pertuz <jedp082@gmail.com>
2021-11-12 09:26:34 -0500

    🧱 Connected to S3 and get a book

--- aws/boto3.py
+++ aws/boto3.py
@@ -1,7 +1,9 @@
 import boto3
 
 class S3:
-    S3 = boto3.resource('s3')
+    S3 = boto3.client('s3')
+    _current_bucket = ''
+    bucket = None
 
     def __init__(self) -> None:
         pass
@@ -10,3 +12,22 @@ class S3:
         buckets = self.S3.buckets.all()
         for bucket in buckets:
             print(bucket.name)
+
+    def set_bucket(self, name: str) -> None:
+        self._current_bucket = name
+
+    def get_bucket(self):
+        self.bucket = self.S3.Bucket(self._current_bucket)
+        return self.bucket
+
+    def get_bucket_objects(self):
+        return self.bucket.objects.all()
+
+    def get_list_buckets(self):
+        s3 = boto3.client('s3')
+        resp = s3.list_buckets()
+        for bucket in resp['Buckets']:
+            print(f'Bucket -> {bucket}')
+
+    def get_object(self, object: str):
+        return self.S3.get_object(Bucket=self._current_bucket, Key=object)
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from aws.boto3 import S3
+import re
+
+def filter_books(objs):
+    str_book = 'lbr/book'
+    if re.match(str_book, objs['key']):
+        return True
+    return False
+
+# Book key
+book_key = 'lbr/book/053179a7fd544f1693cce8a2247bbdb3/OEBPS/modelos_comportamiento_epub-55.xhtml'
 
 s3 = S3()
-s3.get_buckets()
+s3.set_bucket('beereaders.com')
+# s3.get_buckets()
+# buck = s3.get_bucket()
+# objs = buck.objects.all()
+# for obj in objs:
+#     print(obj)
+obj = s3.get_object(book_key)
+print(obj['Body'].read())
